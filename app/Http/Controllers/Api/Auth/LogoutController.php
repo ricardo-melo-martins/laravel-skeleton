e2135c5d8f600ffdac7c9caa17511bd03fe1ae62
⚡RMM⚡ <coreexpress@gmail.com>
2024-04-15 21:04:56 -0300

    fix logout endpoint

--- app/Http/Controllers/Api/Auth/LogoutController.php
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -5,18 +5,14 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    public function __construct()
-    {
-        // $this->middleware('auth:api');
-    }
-
+ 
     public function logout(Request $request): JsonResponse
     {
-        
-        $request->user()->currentAccessToken()->delete();
+        Auth::logout();
         
         return response()->json(['message' => 'Successfully logged out']);
     }
